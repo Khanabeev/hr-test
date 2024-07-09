2443dcf4b494aae98a3f7be60931969679cc6fb6
Anton Khanabeev <khanabeev@gmail.com>
2024-07-09 09:18:37 +0300

    add: cancel action

---
+++ src/Loyalty/Actions/CancelTransactionAction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Src\Loyalty\Actions;
+
+use Src\Loyalty\Exceptions\TransactionNotFoundException;
+use Src\Loyalty\Models\LoyaltyPointsTransaction;
+use Symfony\Component\HttpFoundation\Response;
+
+class CancelTransactionAction
+{
+    /**
+     * @throws TransactionNotFoundException
+     */
+    public static function execute(int $transactionId, string $reason): LoyaltyPointsTransaction
+    {
+        $transaction = LoyaltyPointsTransaction::where('id', '=', $transactionId)
+            ->where('canceled', '=', 0)
+            ->first();
+
+        if (!$transaction) {
+            throw new TransactionNotFoundException(
+                'Transaction is not found',
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $transaction->canceled = time();
+        $transaction->cancellation_reason = $reason;
+        $transaction->save();
+
+        return $transaction;
+    }
+
+}
